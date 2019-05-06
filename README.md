a61ec5758b8bf92bcef8d7b5cd22681dcb53a08a
pabloturati <pablo.turati@gmail.com>
2019-05-06 16:53:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Stack_from_queues
---
+++ Stack_from_queues/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Stack_from_queues
+//
+//  Created by Pablo Turati on 5/6/19.
+//  Copyright © 2019 PabloTurati. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
